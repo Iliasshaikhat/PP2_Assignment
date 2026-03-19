fdb3e4129af6faa0af410b20faccf84646820b03
Iliasshaikhat <i_shaikhat@kbtu.kz>
2026-03-19 11:45:18 +0500

    Add Practice6 - file handling and built-in functions examples

--- Practice06/Task/Built-in functions.py
+++ Practice06/Task/Built-in functions.py
@@ -1,17 +1,49 @@
 # Libraries
 
+from  functools import reduce
 
 # Exercise 1
 
+nums = int(input())
 
+squares = list(map(lambda x : x**2, nums))
+print(squares)
+sorted = list(filter(lambda x : x % 2 == 0, squares))
+print(sorted)
 
 # Exercise 2
 
+nums = int(input())
 
-
+total = reduce(lambda x, y : x + y, nums)
+print(total)
 # Exercise 3
 
+names = ["Doner", "Pizza", "Chubar Lagman"]
+
+for i, name in enumerate(names):
+    print(i, name)
 
 
+names = ["Doner", "Pizza", "Chubar Lagman"]
+scores = [990, 1490, 2590]
+
+for name, score in zip(names, scores):
+    print(name, score)
+    
+    
 # Exercise 4
 
+x = 5
+
+print(type(x))  
+print(isinstance(x, int))
+
+a = "10"
+
+num = int(a)   
+print(num + 5) 
+
+b = 5
+text = str(b)
+print(text + " apples")
