d199490d173ef012e771713c30ab30cc6dbb079e
Iliasshaikhat <i_shaikhat@kbtu.kz>
2026-04-01 22:55:12 +0500

    Add Practice7 - PhoneBook with PostgreSQL

--- Test.py
+++ Test.py
@@ -1,8 +1,10 @@
-import re
+with open ("names.txt", "r") as file1:
+    text1 = file1.read()
+    
+with open ("grades.txt", "r") as file2:
+    text2 = file2.read()
+list2 = text2.split()
+list1 = text1.split()
 
-text = input()
-pattern = r"\b[a-zA-Z]{4}\b"
-
-words = re.findall(pattern,text)
-
-print(words)
+for n , g in zip(list1,list2 ) :
+    print (f'{n}-{g}')
